fa946ef8274a85812fa9aa863b728e1cac9eaaf3
Srishti <srishti.prasad@bluestone.com.au>
2021-05-01 16:36:18 +1000

    Changed the AMI

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
 	region = "ap-southeast-2"
 }
-
 resource "aws_instance" "myserver" {
-	ami = "ami-0c55b159cbfafe1f0"
+	ami = "ami-09624659bc7805447"
     instance_type = "t2.micro"
 }
