e4a44d7bfde6331117682d1e02e6b66dc80909ab
Srishti <srishti.prasad@bluestone.com.au>
2021-05-01 15:06:20 +1000

    deploy a single server

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = "ap-southeast-2"
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "myserver" {
 	ami = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2-micro"
+    instance_type = "t2.micro"
 }
