19719f0861979436f3fa21ce776145538a497954
Srishti <srishti.prasad@bluestone.com.au>
2021-05-11 17:09:34 +1000

    refreshed

--- demo/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {
-   default =   "ap-southeast-2"
-}
-
-variable "vpc_azs" {
-  description = "List of Subnet AZs"
-  default     = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
-}
